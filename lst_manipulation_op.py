c2a961a7d68734e0aeebfe08e058ce938f5c7ab8
Sourav <souravghosh251@outlook.com>
2026-05-07 13:46:05 +0530

    adding the changes

---
+++ lst_manipulation_op.py
@@ -0,0 +1,10 @@
+lst = [1,2,3,4,5,6]
+
+lst1 = [7,8,9,10]
+output= lst + [lst1]
+print(output)
+output1 = lst + lst1
+print(output1)
+
+output = [1,2,3,4,5,6,[7,8,9,10]]
+output1 = [1,2,3,4,5,6,7,8,9,10]
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Map Filter and Reduce Python
+
+
